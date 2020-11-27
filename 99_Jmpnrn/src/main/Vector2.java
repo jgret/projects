d7c735e38229265ebfcf6593c038c91298a0ab3a
Salzkammergut <63403332+jgret@users.noreply.github.com>
2020-11-27 16:17:20 +0100

    copyright

--- 99_Jmpnrn/src/main/Vector2.java
+++ 99_Jmpnrn/src/main/Vector2.java
@@ -1,3 +1,11 @@
+/*******************************************************
+ * Copyright (C) 2020-2021 jgret <thomgreimel@gmail.com>
+ * 
+ * This file is part of Jmpnrn.
+ * 
+ * Jmpnrn can not be copied and/or distributed without the express
+ * permission of Greimel jgret
+ *******************************************************/
 package main;
 
 /**
