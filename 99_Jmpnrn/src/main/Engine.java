ed9c2e04ee3ece2ef775848e778d31bc2e06f442
Salzkammergut <63403332+jgret@users.noreply.github.com>
2020-12-10 09:56:00 +0100

    enhanced gameloop

--- 99_Jmpnrn/src/main/Engine.java
+++ 99_Jmpnrn/src/main/Engine.java
@@ -12,90 +12,57 @@ import java.awt.*;
 
 public abstract class Engine implements Runnable {
 
-    protected Input input;
-    protected GameScreen screen;
-    protected double frameTime;
-    private double precisionThreshold = 0.004;
-    private volatile boolean running;
+	protected Input input;
+	protected GameScreen screen;
+	protected double frameTime;
+	private double precisionThreshold = 0.004;
+	private volatile boolean running;
 
-    public Engine(int width, int height, int scale, int fps) {
-        System.out.println("FPS set to " + (fps < 0 ? "Unlimited" : fps));
-        this.frameTime = 1.0 / fps;
-        this.input = new Input();
-        this.screen = new GameScreen(this, width, height, scale);
-        Image2d.makeContext(screen);
-        this.screen.addKeyListener(input);
-        this.screen.addMouseListener(input);
-        this.screen.addMouseMotionListener(input);
-        this.screen.addMouseWheelListener(input);
-    }
+	public Engine(int width, int height, int scale, int fps) {
+		System.out.println("FPS set to " + (fps < 0 ? "Unlimited" : fps));
+		this.frameTime = 1.0 / fps;
+		this.input = new Input();
+		this.screen = new GameScreen(this, width, height, scale);
+		Image2d.makeContext(screen);
+		this.screen.addKeyListener(input);
+		this.screen.addMouseListener(input);
+		this.screen.addMouseMotionListener(input);
+		this.screen.addMouseWheelListener(input);
+	}
 
-    public void start() {
-        Thread game = new Thread(this);
-        screen.setVisible(true);
-        game.start();
-    }
+	public void start() {
+		Thread game = new Thread(this);
+		screen.setVisible(true);
+		game.start();
+	}
 
-    @Override
-    public void run() {
+	@Override
+	public void run() {
 
-        running = true;
-        double last = Time.getTime();
-        double delta = 0;
-        double lastDelta = Time.getTime();
+		running = true;
 
-        this.init();
+		this.init();
 
-        if (frameTime > 0) {
+		double last = Time.getTime();
+		while (running) {
+			double now = Time.getTime();
+			double elapsedTime = now - last;
+			last = now;
+			this.input.poll();
+			this.update(elapsedTime);
+			this.screen.render();
+		}
 
-            while (running) {
+	}
 
-                double now = Time.getTime();
-                double elapsedTime = now - last;
-                last = now;
+	public abstract void init();
 
-                while (delta <= frameTime) {
-                    double nowDelta = Time.getTime();
-                    delta += nowDelta - lastDelta;
-                    lastDelta = nowDelta;
-                }
+	public abstract void update(double elapsedTime);
 
-                while (delta >= frameTime) {
+	public abstract void render(Graphics2D g2, Camera cam, int scale);
 
-                    this.input.poll();
-                    this.update(elapsedTime);
-                    this.screen.render();
-
-                    delta -= frameTime;
-                }
-
-            }
-        } else {
-
-            while (running) {
-
-                double now = Time.getTime();
-                double elapsedTime = now - last;
-                last = now;
-
-                this.input.poll();
-                this.update(elapsedTime);
-                this.screen.render();
-
-            }
-
-        }
-
-    }
-
-    public abstract void init();
-
-    public abstract void update(double elapsedTime);
-
-    public abstract void render(Graphics2D g2, Camera cam, int scale);
-
-    public Input getInput () {
-        return this.input;
-    }
+	public Input getInput () {
+		return this.input;
+	}
 
 }
