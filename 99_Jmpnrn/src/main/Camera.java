7182c8445289211e377cece53457780de093c101
Salzkammergut <63403332+jgret@users.noreply.github.com>
2020-12-10 09:55:17 +0100

    cam

--- 99_Jmpnrn/src/main/Camera.java
+++ 99_Jmpnrn/src/main/Camera.java
@@ -8,10 +8,39 @@
  *******************************************************/
 package main;
 
-public class Camera extends Rectangle{
+public class Camera extends Rectangle {
 	
-    public Camera(double x, double y, double width, double height) {
+	private GameObject target;
+	private GameScreen screen;
+	
+    public Camera(GameScreen screen, double x, double y, double width, double height) {
         super(x, y, width, height);
+        this.screen = screen;
+    }
+    
+    public void setTarget(GameObject target) {
+    	this.target = target;
+    }
+    
+    public void update(double elapsedTime) {
+    	Vector2 screen = this.getDim();
+    	Rectangle world = target.getWorldIn().getBounds();
+    	int scale = this.screen.getScale();
+
+    	
+    	
+    	this.pos.x = target.getX();
+    	this.pos.y = target.getY();
+    	this.pos.set(0, 0);
+    	
+    }
+    
+    public double getX(double scale) {
+    	return this.pos.x * scale;
+    }
+    
+    public double getY(double scale) {
+    	return this.pos.y * scale;
     }
 
 }
