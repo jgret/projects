b3d8889ca2efd9774de683561dcb683f4080c5a1
Salzkammergut <63403332+jgret@users.noreply.github.com>
2020-12-10 09:57:44 +0100

    playable character

--- 99_Jmpnrn/src/main/Game.java
+++ 99_Jmpnrn/src/main/Game.java
@@ -11,26 +11,50 @@ package main;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Game extends Engine {
 
     public Game() {
-        super(1280, 720, 32, 60);
+        super(1280, 720, 32, -1);
     }
     
     private World world;
+    private Player player;
 
     @Override
     public void init() {
-    	world = new World();
+    	
+    	world = new World(this);
     	world.load("default");
+    	world.init();
+    	
+    	Image2d player_img = null;
+		try {
+			
+			player_img = new Image2d(ImageIO.read(new File("res/img/world_32x32.png")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+    	player = new Player(world, new Rectangle(3, 3, 1, 1), player_img);
+    	world.spawn(player, new Vector2(3, 3));
+    	
+    	screen.getCam().setTarget(player);
+    	
     }
-    
-    public double rotX;
     
     @Override
     public void update(double elapsedTime) {
-    	
+    	globalHotKeys();
+    	world.update(elapsedTime);
+    	screen.getCam().update(elapsedTime);
+    }
+    
+    public void globalHotKeys() {
     	if (input.keyPressed(KeyEvent.VK_F11)) {
     		if (screen.isFullscreen()) {
     			screen.leaveFullscreen();
@@ -45,51 +69,31 @@ public class Game extends Engine {
     		screen.setScale(scale);
     	} else {
     		
-    		double zoom = screen.getZoom();
-    		zoom += input.wheelRotations() * -0.1;
-    		screen.setZoom(zoom);
+//    		double zoom = screen.getZoom();
+//    		zoom += input.wheelRotations() * -0.1;
+//    		screen.setZoom(zoom);
     		
     	}
-    	
-    	screen.setTitle("Game z: " + String.format("%.1g", screen.getZoom()).replace(',', '.') + " s: " + screen.getScale());
-    	
-    	
-    	rotX += 10 * elapsedTime;
     }
+    
 
     @Override
     public void render(Graphics2D g2, Camera cam, int scale) {
     	
-    	g2.setBackground(Color.CYAN);
-    	
-    	int width = (int) cam.getWidth();
-    	int height = (int) cam.getHeight();
+//    	int width = (int) cam.getWidth();
+//    	int height = (int) cam.getHeight();
     	
     	world.draw(g2, cam, scale);
+    	player.draw(g2, cam, scale);
     	
-//    	g2.translate(width / 2, height / 2);
-//    	g2.rotate(Math.toRadians(rotX));
-//    	
-//    	Font font = new Font("Consolas", Font.ITALIC, 20);
-//    	g2.setFont(font);
-//    	
-//    	Area area = new Area();
-//    	Polygon p = new Polygon();
-//    	p.addPoint(-50, -50);
-//    	p.addPoint( 25, -50);
-//    	p.addPoint( 50, -25);
-//    	p.addPoint( 50,  50);
-//    	p.addPoint(-50,  50);
-//    	p.translate(40, 40);
-//    	
-//    	g2.drawPolygon(p);
-//    	
-//    	g2.setColor(Color.RED);
-//    	g2.drawLine(0, 0, 250, 0);
-//    	g2.drawString("+x", 255, 0);
-//    	g2.setColor(Color.BLUE);
-//    	g2.drawLine(0, 0, 0, 250);
-//    	g2.drawString("+y", 0, 255);
+    	g2.setColor(Color.BLUE);
+    	g2.fillRect((int) (-cam.getX(scale)), (int) (-cam.getY(scale)), (int) scale * 2, (int) scale * 2);
+    	
+    	g2.setColor(Color.BLACK);
+    	g2.drawString("Cam x " + cam.getX(), 10, 10);
+    	g2.drawString("Cam y " + cam.getY(), 10, 20);
+    	g2.drawString("Player x " + player.getX(), 10, 30);
+    	g2.drawString("Player y " + player.getY(), 10, 40);
     	
     	
     	
